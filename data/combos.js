ea4eba962a5557428f5eda85ebe5c8adcbaba82c
Noah Santilio <59184182+noahsantilio@users.noreply.github.com>
2026-03-14 19:19:29 -0300

    Create combos.js

---
+++ data/combos.js
@@ -0,0 +1,27 @@
+const combos=[
+
+{
+name:"Heliod Ballista",
+cards:[
+"Heliod, Sun-Crowned",
+"Walking Ballista"
+]
+},
+
+{
+name:"Thoracle",
+cards:[
+"Thassa's Oracle",
+"Demonic Consultation"
+]
+},
+
+{
+name:"Dockside Sabertooth",
+cards:[
+"Dockside Extortionist",
+"Temur Sabertooth"
+]
+}
+
+]
