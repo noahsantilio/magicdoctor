ea6a9829a624c7e96ed03b05d313d0404db1d86f
Noah Santilio <59184182+noahsantilio@users.noreply.github.com>
2026-03-14 19:32:25 -0300

    Create staples.js

---
+++ data/staples.js
@@ -0,0 +1,93 @@
+const staples = {
+
+colorless:[
+
+"Sol Ring",
+"Arcane Signet",
+"Mana Crypt",
+"Mana Vault",
+"Sensei's Divining Top",
+"Lightning Greaves",
+"Swiftfoot Boots",
+"Skullclamp",
+"Chromatic Lantern",
+"Commander's Sphere"
+
+],
+
+green:[
+
+"Cultivate",
+"Nature's Lore",
+"Three Visits",
+"Rampant Growth",
+"Beast Whisperer",
+"Guardian Project",
+"Heroic Intervention",
+"Finale of Devastation",
+"Worldly Tutor",
+"Eternal Witness"
+
+],
+
+blue:[
+
+"Rhystic Study",
+"Mystic Remora",
+"Counterspell",
+"Cyclonic Rift",
+"Brainstorm",
+"Ponder",
+"Preordain",
+"Force of Will",
+"Fierce Guardianship",
+"Time Warp"
+
+],
+
+black:[
+
+"Demonic Tutor",
+"Vampiric Tutor",
+"Toxic Deluge",
+"Reanimate",
+"Necropotence",
+"Phyrexian Arena",
+"Yawgmoth's Will",
+"Dark Ritual",
+"Exsanguinate",
+"Grim Tutor"
+
+],
+
+red:[
+
+"Dockside Extortionist",
+"Jeska's Will",
+"Wheel of Fortune",
+"Blasphemous Act",
+"Chaos Warp",
+"Deflecting Swat",
+"Faithless Looting",
+"Underworld Breach",
+"Gamble",
+"Valakut Exploration"
+
+],
+
+white:[
+
+"Smothering Tithe",
+"Swords to Plowshares",
+"Path to Exile",
+"Esper Sentinel",
+"Teferi's Protection",
+"Land Tax",
+"Enlightened Tutor",
+"Farewell",
+"Austere Command",
+"Flawless Maneuver"
+
+]
+
+}
