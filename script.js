903193ffa475f0fd2f8761813c553976671b80eb
Noah Santilio <59184182+noahsantilio@users.noreply.github.com>
2026-03-14 18:24:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 async function analyzeDeck(){
 
 const link = document.getElementById("deckLink").value
+const resultBox = document.getElementById("result")
 
 if(!link.includes("moxfield")){
 alert("Use um link do Moxfield")
 return
 }
 
-const deckID = link.split("/decks/")[1]
+try{
+
+const deckID = link.split("/decks/")[1].split("?")[0]
+
+resultBox.textContent = "Lendo deck..."
 
 const response = await fetch(
 `https://api.moxfield.com/v2/decks/all/${deckID}`
@@ -19,7 +24,7 @@ const cards = Object.values(data.mainboard)
 
 let totalCards = 0
 
-cards.forEach(card => {
+cards.forEach(card=>{
 totalCards += card.quantity
 })
 
@@ -27,15 +32,26 @@ const decklist = cards
 .map(card => card.quantity + " " + card.card.name)
 .join("\n")
 
-const result = `
-Commander: ${data.commanders ? Object.values(data.commanders)[0].card.name : "Unknown"}
+const commander =
+data.commanders ?
+Object.values(data.commanders)[0].card.name :
+"Unknown"
+
+resultBox.textContent = `
+Commander: ${commander}
 
 Total de cartas: ${totalCards}
 
 Decklist:
+
 ${decklist}
 `
 
-document.getElementById("result").textContent = result
+}catch(error){
+
+resultBox.textContent =
+"Erro ao ler o deck. Verifique se o link é público."
+
+}
 
 }
