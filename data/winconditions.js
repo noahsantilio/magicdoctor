e117142ec22416e51e534ce1617df461ef5c5b9e
Noah Santilio <59184182+noahsantilio@users.noreply.github.com>
2026-03-14 19:32:45 -0300

    Create winconditions.js

---
+++ data/winconditions.js
@@ -0,0 +1,25 @@
+const winConditions = [
+
+"Thassa's Oracle",
+
+"Approach of the Second Sun",
+
+"Laboratory Maniac",
+
+"Jace, Wielder of Mysteries",
+
+"Felidar Sovereign",
+
+"Test of Endurance",
+
+"Revel in Riches",
+
+"Exsanguinate",
+
+"Torment of Hailfire",
+
+"Craterhoof Behemoth",
+
+"Triumph of the Hordes"
+
+]
