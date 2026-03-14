8fb42e25773c727ced837dd46014db35ea387d02
Noah Santilio <59184182+noahsantilio@users.noreply.github.com>
2026-03-14 19:30:44 -0300

    Update archetypes.js

--- data/archetypes.js
+++ data/archetypes.js
@@ -1,6 +1,8 @@
-const archetypes={
+const archetypes = {
 
-tokens:["create a token"],
+tokens:[
+"create a token"
+],
 
 aristocrats:[
 "sacrifice",
@@ -12,14 +14,95 @@ voltron:[
 "aura"
 ],
 
+stax:[
+"players can't",
+"each player can't"
+],
+
+blink:[
+"exile",
+"return to the battlefield"
+],
+
+reanimator:[
+"return target creature from your graveyard"
+],
+
 counters:[
 "+1/+1 counter"
 ],
 
-stax:[
-"players can't",
-"each player can't"
+spellslinger:[
+"instant",
+"sorcery"
+],
+
+storm:[
+"storm"
+],
+
+treasure:[
+"create a treasure token"
+],
+
+artifacts:[
+"artifact"
+],
+
+enchantress:[
+"enchantment"
+],
+
+lands:[
+"landfall"
+],
+
+mill:[
+"mill"
+],
+
+discard:[
+"discard"
+],
+
+control:[
+"counter target"
+],
+
+combo:[
+"search your library"
+],
+
+tribal:[
+"creature type"
+],
+
+infect:[
+"infect"
+],
+
+extraTurns:[
+"extra turn"
+],
+
+groupHug:[
+"each player draws"
+],
+
+chaos:[
+"at random"
+],
+
+lifegain:[
+"gain life"
+],
+
+equipment:[
+"equip"
+],
+
+graveyard:[
+"graveyard"
 ]
 
 }
-
