3f95e8697accec71a8fe1ec6e2f07e81eb27ab3a
Noah Santilio <59184182+noahsantilio@users.noreply.github.com>
2026-03-14 19:20:04 -0300

    Update archetypes.js

--- data/archetypes.js
+++ data/archetypes.js
@@ -1,1 +1,25 @@
+const archetypes={
+
+tokens:["create a token"],
+
+aristocrats:[
+"sacrifice",
+"dies"
+],
+
+voltron:[
+"equip",
+"aura"
+],
+
+counters:[
+"+1/+1 counter"
+],
+
+stax:[
+"players can't",
+"each player can't"
+]
+
+}
 
